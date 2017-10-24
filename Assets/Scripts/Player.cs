1ed5c14ec094c2cb7f57d3eeaa37da9e86d32ce8
DaltonSmith <djsmith633@gmail.com>
2017-10-24 15:08:56 -0400

    Intial  Project Upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //Floats
+    public float maxSpeed = 3;
+    public float speed = 50f;
+    public float jumpPower = 150f;
+
+
+    //Booleans
+    public bool grounded;
+    public bool canDoubleJump;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+        rb2d = gameObject.GetComponent < Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetBool("Grounded",grounded);
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+
+        if(Input.GetAxis("Horizontal")< -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+
+        //Adds a force to the button , spacebar
+        if (Input.GetButtonDown("Jump") )
+        {
+            if (grounded)
+            {
+                rb2d.AddForce(Vector2.up * jumpPower);
+                canDoubleJump = true;
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    canDoubleJump = false;
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                    rb2d.AddForce(Vector2.up * jumpPower /1.75f);
+                }
+            }
+            
+        }
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+        easeVelocity.x *= 0.75f;
+
+
+        float h = Input.GetAxis("Horizontal");
+
+
+        //Fake friction , easing the x speed of our player
+        if (grounded)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+
+
+        // We are getting the arrow direction and multiplying it by the speed 
+        rb2d.AddForce((Vector2.right * speed)*h);
+
+
+        //Limiting the speed of the player
+        if(rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed,rb2d.velocity.y);
+        }
+
+        if(rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+    }
+
+
+
+}
